efd567a3daf97a7fc79a5cdcd6077126e50cb644
Alex Goessmann <alex.goessmann@web.de>
2026-07-30 16:22:24 +0200

    Added compile file

---
+++ papers/maximum-entropy/maxEnt-compile.sh
@@ -0,0 +1,15 @@
+cd arXiv-preprint
+pdflatex arXiv-maxEnt
+bibtex arXiv-maxEnt
+pdflatex arXiv-maxEnt
+
+cd ../wias-preprint
+pdflatex wias-maxEnt
+bibtex wias-maxEnt
+pdflatex wias-maxEnt
+pdflatex wias-maxEnt
+
+cd ../sn-submission
+pdflatex sn-maxEnt
+bibtex sn-maxEnt
+pdflatex sn-maxEnt
